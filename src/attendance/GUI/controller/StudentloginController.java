f2dafce48855294c128c4fca3cb88aca2744a7a1
fjt1509 <fjt1509@hotmail.com>
2018-02-14 03:01:49 +0100

    start

--- src/attendance/GUI/controller/StudentloginController.java
+++ src/attendance/GUI/controller/StudentloginController.java
@@ -5,11 +5,18 @@
  */
 package attendance.GUI.controller;
 
+import com.jfoenix.controls.JFXButton;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.ResourceBundle;
+import javafx.animation.FadeTransition;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
 
 /**
  * FXML Controller class
@@ -25,14 +32,53 @@ public class StudentloginController implements Initializable
     private Label dateLbl;
     @FXML
     private Label welcomelbl;
+    @FXML
+    private AnchorPane rootPane;
+    @FXML
+    private JFXButton submitAttendanceBtn;
+    @FXML
+    private Label registerSuccesfulLbl;
 
+    private Date date = new Date();
+    private SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+    
+    
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb)
     {
-        // TODO
+        rootPane.setOpacity(0);
+        registerSuccesfulLbl.setVisible(false);
+        makeFadingTransmition();
+        
+        
+        welcomelbl.setText("Welcome Daniel Saaby Rasmussen");
+        dateLbl.setText("Current date: " + sdf.format(date));
+        classLbl.setText("Current class: SCO");
+        
+  
+        
     }    
+
+    @FXML
+    private void submitAttendanceEvent(ActionEvent event) 
+    {
+        submitAttendanceBtn.setVisible(false);
+        registerSuccesfulLbl.setVisible(true);
+        
+    }
+
+    private void makeFadingTransmition() 
+    {
+        FadeTransition fadeTransition = new FadeTransition();
+        fadeTransition.setDuration(Duration.millis(2500));
+        fadeTransition.setNode(rootPane);
+        fadeTransition.setFromValue(0);
+        fadeTransition.setToValue(1);
+        fadeTransition.play();        
+    }
     
 }
